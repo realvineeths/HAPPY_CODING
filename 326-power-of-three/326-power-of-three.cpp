12b4b9d983f947ca32aa42be176fe63725013a58
realvineeths <85937627+realvineeths@users.noreply.github.com>
2022-08-24 09:52:01 +0530

    Time: 50 ms (15.85%), Space: 5.9 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -7,10 +7,12 @@
 // };
 
 
-class Solution { //since 3 is a prime number we could use this method
+class Solution { //since 3 is a prime number we could use this method 
 public:
     bool isPowerOfThree(int n) {
         
         return n>0 && (int)pow(3,19)%n==0;       
     }
 };
+
+//if in case of power of 7 we could do 7^(log10(2^32-1/2)/log10(7))/n==0
