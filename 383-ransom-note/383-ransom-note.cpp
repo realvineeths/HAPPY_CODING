01aa4bad19c2f69a675d9920841937ddfbb6ab00
realvineeths <85937627+realvineeths@users.noreply.github.com>
2022-08-25 18:45:42 +0530

    Time: 24 ms (56.74%), Space: 8.6 MB (89.81%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        map<char,int>m1;
+        // map<char,int>m1;
+        int m1[26]={0};
         
         for(auto a:magazine)
         {
-            m1[a]++;            
+            m1[a-'a']++;            
         }
         
         for(auto c:ransomNote)
         {
-            if(m1[c]--==0)  return false;
+            if(m1[c-'a']--==0)  return false;
             
         }
         return true;
