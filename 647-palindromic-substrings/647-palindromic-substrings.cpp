d13d5d5b78ea9b2e3a410c4ea2cf9c501382f3b0
realvineeths <85937627+realvineeths@users.noreply.github.com>
2022-09-07 16:01:03 +0530

    Time: 1739 ms (5.01%), Space: 418.2 MB (7.30%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -13,14 +13,47 @@ public:
         
     }
     
-    int countSubstrings(string s) {
+    int countSubstringshelp(string s) {
         
         int ans=0;
         for(int i=0;i<s.size();i++)
         {
             for(int j=i;j<s.size();j++)
             {
+
                 if(helper(s,i,j))   ans++;
+            }
+        }
+        return ans;
+    }
+    
+    int helperMem(string s,int i,int j,vector<vector<int>>&dem)
+    {
+        if(dem[i][j]>=0)   return dem[i][j];
+        while(i<j)
+        {
+            if(s[i]!=s[j])  return dem[i][j]=0;
+            i++;
+            j--;
+        }
+        return dem[i][j]=1;
+        
+        // if (i >= j) return 1;
+        // if (mem[i][j] >= 0) return mem[i][j];
+        // return mem[i][j] = s[i] == s[j] ? helperMem(s, i+1, j-1,mem) : 0;
+        
+    }
+    
+    int countSubstrings(string s) {
+        vector<vector<int>>dem(s.size(),vector<int>(s.size(),-1));
+        
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=i;j<s.size();j++)
+            {
+
+                if(helperMem(s,i,j,dem))   ans++;
             }
         }
         return ans;
