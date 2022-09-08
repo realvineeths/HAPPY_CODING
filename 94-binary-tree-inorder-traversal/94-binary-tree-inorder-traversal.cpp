54a1b4b7641d4d79ee6d8b0016040e808bdf6d9d
realvineeths <85937627+realvineeths@users.noreply.github.com>
2022-09-08 19:16:52 +0530

    Time: 10 ms (5.11%), Space: 8.5 MB (39.59%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,18 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    stack<TreeNode*>st;
-    vector<int>ans;
+// class Solution {
+// public:
+//     stack<TreeNode*>st;
+//     vector<int>ans;
     
+//     vector<int> inorderTraversal(TreeNode* root) {
+        
+//         if(!root)   return {};
+        
+//         inorderTraversal(root->left);
+//         ans.push_back(root->val);
+//         inorderTraversal(root->right);
+        
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:    
     vector<int> inorderTraversal(TreeNode* root) {
         
-        if(!root)   return {};
-        
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
+        stack<TreeNode*>st;
+        vector<int>ans;
+
+        while(root or !st.empty())
+        {
+            while(root)
+            {
+                st.push(root);
+                root=root->left;                
+            }
+            root=st.top();
+            st.pop();
+            ans.push_back(root->val);
+            root=root->right;
+        }        
         
         return ans;
     }
