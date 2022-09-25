25cddf1494545b2a2cfd2a7dda1e8830fcb8fdfe
realvineeths <85937627+realvineeths@users.noreply.github.com>
2022-09-25 19:00:10 +0530

    Time: 56 ms (21.95%), Space: 16.9 MB (48.83%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -0,0 +1,115 @@
+// class MyCircularQueue {
+// private:
+//         vector<int>q;
+//         int head=0,tail=0,size=0,maxsz=0;
+// public:
+//     MyCircularQueue(int k) {
+//         q.resize(k);     
+//         maxsz=k;
+        
+//     }
+    
+//     bool enQueue(int value) {
+//         if(isFull())  return false;
+//         if(isEmpty())
+//         {
+//             q[tail]=value;
+//             size++;       
+//             return true;
+//         }
+        
+//         tail++;
+//         tail%=maxsz;
+//         q[tail]=value;
+//         size++;
+//         return true;    
+    
+//     }
+    
+//     bool deQueue() {
+        
+//         if(isEmpty())   return false;
+        
+//         head=(head+1)%maxsz;
+//         size--;
+//         return true;
+//     }
+    
+//     int Front() {
+        
+//         return isEmpty()?-1:q[head];
+//     }
+    
+//     int Rear() {
+//         return isEmpty() ? -1 : q[tail];
+//     }
+    
+//     bool isEmpty() {
+        
+//         return size==0;
+//     }
+    
+//     bool isFull() {
+//         return size==maxsz;
+//     }
+// };
+
+
+
+class MyCircularQueue {
+public:
+    MyCircularQueue(int k){ arraysize=k; cq.resize(k); }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        if(isEmpty()){ head=tail=0; cq[tail] = value;  size++; return true; }
+        tail++;
+         tail %= arraysize;
+        cq[tail] = value;
+       
+       
+        size++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        head = (head + 1) % arraysize;
+        size--;
+        return true;
+    }
+    
+    int Front() {
+        return isEmpty() ? -1 : cq[head];
+    }
+    
+    int Rear() {
+        return isEmpty() ? -1 : cq[tail];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == arraysize;
+    }
+    
+private:
+    int head=0, tail=0, size=0, arraysize=0;
+    vector<int> cq;
+};
+
+
+
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
